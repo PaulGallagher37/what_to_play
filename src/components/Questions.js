bc9a419e90b384db5ba2f0525da1c2ef703475b5
Paul G <paulgallagher37@yahoo.co.uk>
2025-09-08 20:59:15 +0100

    adding question generator

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,32 +1,34 @@
-import React from "react";
+
 import { useState } from "react";
-
-
-const QuestionGenerator = () => {
-
-const Question_List = [
-    "Question One",
-    "Question Two",
-    "Question Three",
-];
-
-const [currentQuestion, setCurrentQuestion] = useState("");
-
-const generateQuestion = () => {
-    const randomIndex = Math.floor(Math.random() * Question_List.length);
-    setCurrentQuestion(Question_List[randomIndex])
-}
-
-};
 
 
 
 function Questions() {
+
+
+const [currentQuestion, setCurrentQuestion] = useState({});
+
+
+const generateQuestion = () => {
+    const Question_List = [
+    {question: "Question One"},
+    {question: "Question Two"},
+    {question: "Question Three"},
+    {question: "Question Four"}
+];
+    const Index = Math.floor(Math.random() * Question_List.length);
+    setCurrentQuestion(Question_List[Index])
+}
+
+
     return (
         <div className="container question-container">
-            
+            <h1 className="questions-h1">{currentQuestion.question}</h1>
+            <input></input>
+            <button onClick={() => generateQuestion()}>Next Question</button>
         </div>
     )
+
 };
 
 
