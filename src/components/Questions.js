77f9190e5194908c6144c1b71e23a8cc9fed2377
Paul G <paulgallagher37@yahoo.co.uk>
2025-09-13 20:44:51 +0100

    adding input functionality

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,35 +2,58 @@
 import { useState } from "react";
 
 
-
 function Questions() {
 
-
-const [currentIndex, setCurrentIndex] = useState(0);
-
 const Question_List = [
-    {question: "Question One"},
-    {question: "Question Two"},
-    {question: "Question Three"},
-    {question: "Question Four"},
-    {question: "Question Five"}
+    "What's the last game you played?",
+    "Do you prefer single or multi-player?",
+    "In the mood for retro, or modern?",
+    "Quick burst, or long sprawling adventure?",
+    "What's your favourite genre?",
 ];
 
+const shuffleArray = (arr) => [...arr].sort(() => Math.random() - 0.5);
+
+const [questions, setQuestions] = useState(shuffleArray(Question_List));
+const [current, setCurrent] = useState(null);
+const [inputValue, setInputValue] = useState("");
+const [answers, setAnswers] = useState([])
 
 const generateQuestion = () => {
-    if (currentIndex < Question_List.length + 1){
-        setCurrentIndex(currentIndex + 1)
-    } 
-    
+    if (questions.length === 0) {
+        setCurrent("That's all folks!");
+        return;
+    }
+    const [first, ...rest] = questions;
+    setCurrent(first);
+    setQuestions(rest);
+};
+
+const handleSubmitAnswer = () => {
+    if (inputValue.trim() !== "") {
+    setAnswers([...answers, inputValue]);
+    setInputValue("");
+    generateQuestion();
+    };
 };
 
 
 
     return (
         <div className="container question-container">
-            <h1 className="questions-h1">{Question_List[currentIndex].question}</h1>
-            <input></input>
-            <button onClick={() => generateQuestion()}>Next Question</button>
+            <h1 className="questions-h1">{current || "Click the button to start!"}</h1>
+            <input 
+                className="questions-input" 
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            >
+            </input>
+            <button 
+                className=" btn btn-danger questions-button" 
+                onClick={() => handleSubmitAnswer()}
+            >Submit
+            </button>
         </div>
     )
 
