1e5927b851a14d2b70d4dfd40e3b77836f7cd519
Paul G <paulgallagher37@yahoo.co.uk>
2025-09-21 15:48:38 +0100

    Adding radio selection

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -5,58 +5,55 @@ import { useState } from "react";
 function Questions() {
 
 const Question_List = [
-    "What's the last game you played?",
-    "Do you prefer single or multi-player?",
-    "In the mood for retro, or modern?",
-    "Quick burst, or long sprawling adventure?",
-    "What's your favourite genre?",
+    {question: "Do you prefer single or multi-player?", 
+        answers: ["Single", "Multiplayer"]},
+    {question: "Are you in the mood for retro, or modern?", 
+        answers: ["Retro", "Modern"]},
+    {question: "What's your ideal gaming session length when you have free time?", 
+        answers: ["Long sessions exploring vast worlds", "Quick high-intensity matches", "Leisurely, relaxed play"]},
+    {question: "What's your favourite genre?", 
+        answers: ["Fighting", "Action", "Adventure", "RPG", "Metroidvania", "Platformer", "FPS", "Puzzle", "Sports", "Racing", "Horror", "Stealth"]},
+    {question: "What setting appeals to you most?", 
+        answers: ["Mythical unerworld", "Futuristic battlefield", "Sprawling Fantasy Realm", "Historical Era", "Cozy Countryside"]},
+    {question: "How important is replayability to you?", 
+        answers: ["I enjoy slow-paced repeat sessions", "I want infinite run-based replays", "Somereplya is fine, but story matters most", "I play competitive modes endlessly"]},
+    {question: "Choose your favourite in-game activity.", 
+        answers: ["Facing randomised challenges", "Exploring hidden caves and ruins", "Fishing and Farming", "Co-ordinating team skirmishes"]},
+    {question: "Pick a visual style that draws you in.", 
+        answers: ["Sleek and futuristic", "Lush, vibrant fantasy landscapes", "Pixel art or hand-drawn", "Stylised mythic or underworld aesthetic"]}
 ];
 
-const shuffleArray = (arr) => [...arr].sort(() => Math.random() - 0.5);
+const [answers, setAnswers] = useState([]);
+const [currentIndex, setCurrentIndex] = useState(0)
 
-const [questions, setQuestions] = useState(shuffleArray(Question_List));
-const [current, setCurrent] = useState(null);
-const [inputValue, setInputValue] = useState("");
-const [answers, setAnswers] = useState([])
 
-const generateQuestion = () => {
-    if (questions.length === 0) {
-        setCurrent("That's all folks!");
-        return;
+
+const nextQuestion = () => {
+    if (currentIndex < Question_List.length -1){
+    setCurrentIndex(currentIndex + 1)
+    } else {
+        setCurrentIndex(currentIndex)
     }
-    const [first, ...rest] = questions;
-    setCurrent(first);
-    setQuestions(rest);
-};
-
-const handleSubmitAnswer = () => {
-    if (inputValue.trim() !== "") {
-    setAnswers([...answers, inputValue]);
-    setInputValue("");
-    generateQuestion();
-    };
-};
+}
 
 
 
     return (
         <div className="container question-container">
-            <h1 className="questions-h1">{current || "Click the button to start!"}</h1>
-            <input 
-                className="questions-input" 
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-            >
-            </input>
-            <button 
-                className=" btn btn-danger questions-button" 
-                onClick={() => handleSubmitAnswer()}
-            >Submit
-            </button>
+                <div>
+                    <h1 className="questions-h1">{Question_List[currentIndex].question}</h1>
+                    <ul>
+                        {Question_List[currentIndex].answers.map((answer) => (
+                            <div className="container">
+                                <input type="radio" value={answer}></input>
+                                <label className="questions-label">{answer}</label>
+                            </div>
+                        ))}
+                    </ul>
+                </div>
+              <button className="btn btn-primary" onClick={nextQuestion}>Next</button>
         </div>
     )
-
 };
 
 
