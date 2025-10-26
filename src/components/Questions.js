944e601c045feef68f9872d76cfa77397cf83d57
Paul G <paulgallagher37@yahoo.co.uk>
2025-10-26 15:14:57 +0000

    refining the question capture logic

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -8,25 +8,31 @@ function Questions() {
 
 const Question_List = [
     "Do you prefer Single Player or Multi-Player?",
-    "Are you in the mood for retro, or modern?",
+    "Are you in the mood for Retro or Modern?",
     "What's your ideal gaming session length when you have free time?", 
     "What's your favourite genre?", 
-    "Pick a visual style that draws you in.",  
+    "What was the last game you really enjoyed playing?",  
 ];
 
 const [currentIndex, setCurrentIndex] = useState(0);
 const [prompt, setPrompt] = useState([]);
 const [recommendations, setRecommendations] = useState("");
+const [input, setInput] = useState("");
 const navigate = useNavigate();
 
 
 
-const nextQuestion = () => {
+const nextQuestion = (event) => {
     if (currentIndex < Question_List.length -1){
     setCurrentIndex(currentIndex + 1)
     } else {
         setCurrentIndex(currentIndex)
     }
+    if (input){
+     setPrompt([...prompt, input])
+    } 
+    setInput("")
+    console.group(prompt);
 }
 
 const previousQuestion = () => {
@@ -38,8 +44,7 @@ const previousQuestion = () => {
 }
 
 const captureAnswer = (event) => {
-    const value = event.target.value;
-    setPrompt([...prompt, value])
+    setInput(event.target.value)
 }
 
 
@@ -67,21 +72,21 @@ async function handleSubmit(e) {
 }
 
     return (
-        <div className="container question-container">
+        <form className="container question-container">
                 <div>
                     <h1 className="questions-h1">{Question_List[currentIndex]}</h1>
                     <ul  className="grid-container">
                         <li className="grid-item" >
-                            <input type="text" onChange={captureAnswer}></input>
+                            <input type="text" value={input} onChange={captureAnswer}></input>
                         </li>    
                     </ul>
                 </div>
                 <div>
-                    <button className="btn btn-primary questions-button" onClick={previousQuestion} disabled={prompt.length === Question_List.length}>Previous</button>
-                    <button className="btn btn-primary questions-button" onClick={nextQuestion} disabled={prompt.length === Question_List.length}>Next</button>
+                    <button type="button" className="btn btn-primary questions-button" onClick={previousQuestion} disabled={prompt.length === Question_List.length}>Previous</button>
+                    <button type="button" className="btn btn-primary questions-button" onClick={nextQuestion} disabled={prompt.length === Question_List.length}>Next</button>
                 </div>
                 <button className="btn btn-primary questions-button-submit" onClick={handleSubmit} disabled={prompt.length !== Question_List.length}>Submit Answers</button>
-        </div>
+        </form>
     )
 };
 
